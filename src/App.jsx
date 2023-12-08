48bffe04365112061251268b75ce7b9d0bcf69e4
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-08 02:21:08 +0200

    reset style, display background successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import "./App.css";
 import Background from "./components/Background/Background";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
       <Background />
     </div>
   );
