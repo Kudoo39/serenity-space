07203fb890ea1c17c235a80a8b3a18a49024e409
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-12 18:47:33 +0200

    adding change background, figure out for it to work later

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Background.css";
 import Day from "../../assets/city/day.mp4";
+import Rain from "../../assets/city/rain.mp4";
 
 const Background = () => {
+  const [weather, setWeather] = useState(Day);
+
+  const changeWeather = () => {
+    setWeather((weather) => {
+      return weather === Day ? Rain : Day;
+    });
+  };
+
   return (
     <div className="background-container">
+      <button onClick={changeWeather}>Boom!</button>
       <video className="video" autoPlay loop muted>
-        <source src={Day} type="video/mp4" />
+        <source src={weather} type="video/mp4" />
       </video>
     </div>
   );
