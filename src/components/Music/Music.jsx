666e5f75273da991f2060a7d8b0cb328505ef63c
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-12 15:25:04 +0200

    updating list of music, skip music feature, still some bugs

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -1,10 +1,44 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./Music.css";
-import { IoIosPlayCircle } from "react-icons/io";
+import {
+  IoIosPlayCircle,
+  IoIosSkipBackward,
+  IoIosSkipForward,
+} from "react-icons/io";
 import { FaRegPauseCircle } from "react-icons/fa";
 
 const Music = () => {
-  const musicRef = useRef(new Audio("../../public/music/music.mp3"));
+  const musicList = [
+    "music.mp3",
+    "music1.mp3",
+    "music2.mp3",
+    "music3.mp3",
+    "music4.mp3",
+    "music5.mp3",
+    "music6.mp3",
+    "music7.mp3",
+    "music8.mp3",
+    "music9.mp3",
+    "music10.mp3",
+    "music11.mp3",
+    "music12.mp3",
+    "music13.mp3",
+    "music14.mp3",
+    "music15.mp3",
+    "music16.mp3",
+    "music17.mp3",
+    "music18.mp3",
+    "music19.mp3",
+    "music20.mp3",
+    "music21.mp3",
+    "music22.mp3",
+    "music23.mp3",
+    "music24.mp3",
+    "music25.mp3",
+    "music26.mp3",
+  ];
+  const [index, setIndex] = useState(0);
+  const musicRef = useRef(new Audio(`../../public/music/${musicList[index]}`));
   const [isPlay, setPlay] = useState(false);
 
   useEffect(() => {
@@ -17,20 +51,40 @@ const Music = () => {
     };
   }, []);
 
+  useEffect(() => {
+    musicRef.current = new Audio(`../../public/music/${musicList[index]}`);
+    setPlay(isPlay);
+    isPlay ? musicRef.current.play() : musicRef.current.pause();
+  }, [index]);
+
+  useEffect(() => {
+    isPlay ? musicRef.current.play() : musicRef.current.pause();
+  }, [isPlay]);
+
   const playMusic = () => {
     setPlay(!isPlay);
   };
 
-  isPlay ? musicRef.current.play() : musicRef.current.pause();
+  const previousMusic = () => {
+    setIndex((index) => (index - 1 + musicList.length) % musicList.length);
+    console.log("previous");
+  };
+
+  const nextMusic = () => {
+    setIndex((index) => (index + 1) % musicList.length);
+    console.log("next");
+  };
 
   return (
     <div className="music-container">
       <div className="button">
+        <IoIosSkipBackward className="music-icon" onClick={previousMusic} />
         {isPlay ? (
           <FaRegPauseCircle className="music-icon" onClick={playMusic} />
         ) : (
           <IoIosPlayCircle className="music-icon" onClick={playMusic} />
         )}
+        <IoIosSkipForward className="music-icon" onClick={nextMusic} />
       </div>
     </div>
   );
