92f7cf43da72fd4953e62c1311cf8c2a52baf50e
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-12 21:24:35 +0200

    customizing something

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -2,21 +2,24 @@ import React, { useState } from "react";
 import "./Background.css";
 import Day from "../../assets/city/day.mp4";
 import Rain from "../../assets/city/rain.mp4";
+import { IoRainy } from "react-icons/io5";
 
 const Background = () => {
   const [weather, setWeather] = useState(Day);
 
   const changeWeather = () => {
-    console.log("Changing weather...");
     setWeather((currentWeather) => {
-      console.log("Current Weather:", currentWeather);
       return currentWeather === Day ? Rain : Day;
     });
   };
 
   return (
     <div className="background-container">
-      <button onClick={changeWeather}>Boom!</button>
+      <div className="button-container">
+        <div onClick={changeWeather}>
+          <IoRainy className="weather-icon" />
+        </div>
+      </div>
       <video key={weather} className="video" autoPlay loop muted>
         <source src={weather} type="video/mp4" />
       </video>
