e01dcc3965edaf575ecf0a96266ee3ef10d07bd3
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-12 22:01:23 +0200

    removing lazyload

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -3,7 +3,6 @@ import "./Background.css";
 import Day from "../../assets/city/day.mp4";
 import Rain from "../../assets/city/rain.mp4";
 import { IoRainy } from "react-icons/io5";
-import LazyLoad from "react-lazy-load";
 
 const Background = () => {
   const [weather, setWeather] = useState(Day);
@@ -21,11 +20,9 @@ const Background = () => {
           <IoRainy className="weather-icon" />
         </div>
       </div>
-      <LazyLoad height={762} offset={300}>
-        <video key={weather} className="video" autoPlay loop muted>
-          <source src={weather} type="video/mp4" />
-        </video>
-      </LazyLoad>
+      <video key={weather} className="video" autoPlay loop muted>
+        <source src={weather} type="video/mp4" />
+      </video>
     </div>
   );
 };
