fe979336896c8b356bd5cb3fca2f505c57aa4048
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-13 17:14:29 +0200

    trying updating with rain sound

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Background.css";
 import Day from "../../assets/city/day.mp4";
 import Rain from "../../assets/city/rain.mp4";
-import { IoRainy } from "react-icons/io5";
+import { IoRainy, IoSunny } from "react-icons/io5";
+import { FaSun, FaCloudRain } from "react-icons/fa6";
 
 const Background = () => {
   const [weather, setWeather] = useState(Day);
+  const raining = new Audio("/sound/rain.mp3");
 
   const changeWeather = () => {
     setWeather((currentWeather) => {
@@ -13,11 +15,21 @@ const Background = () => {
     });
   };
 
+  useEffect(() => {
+    raining.loop = true;
+  }, []);
+
+  weather === Day ? raining.pause() : raining.play();
+
   return (
     <div className="background-container">
       <div className="button-container">
         <div onClick={changeWeather}>
-          <IoRainy className="weather-icon" />
+          {weather === Rain ? (
+            <IoSunny className="weather-icon sun" />
+          ) : (
+            <FaCloudRain className="weather-icon" />
+          )}
         </div>
       </div>
       <video key={weather} className="video" autoPlay loop muted>
