489179f5a595874862d2d16aefec5aa2e7cfc23c
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-12-28 03:09:49 +0200

    try this loop

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -67,19 +67,21 @@ const Music = () => {
     musicRef.current = new Audio(`/music/${musicList[index]}`)
     const music = musicRef.current
     isPlay ? music.play() : music.pause()
-    if (isLoop === true) {
-      return () => {
-        music.pause()
-        music.currentTime = 0
-      }
+    const handleEnd = () => {
+      music.pause()
+      music.currentTime = 0
     }
 
-    music.addEventListener('ended', nextMusic)
+    if (isLoop) {
+      music.addEventListener('ended', handleEnd)
+    } else {
+      music.addEventListener('ended', nextMusic)
+    }
 
     return () => {
       music.pause()
       music.currentTime = 0
-      music.removeEventListener('ended', nextMusic)
+      music.removeEventListener('ended', isLoop ? handleEnd : nextMusic)
     }
   }, [index])
 
